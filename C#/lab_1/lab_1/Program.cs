309f6398074130a9c129c144e98d79d45784a009
CyberCountry <59829069+CyberCountry@users.noreply.github.com>
2020-09-16 19:07:01 +0300

    MyLB1-2

---
+++ C#/lab_1/lab_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+
+namespace lab_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const double Pi = 3.14159265;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Заданный радиус: ");
+            string radius = Console.ReadLine();
+            double r = Convert.ToDouble(radius);
+            double s = 4 * Pi * r * r;
+            double v = (4 / 3) * Pi * r * r * r;
+            Console.WriteLine($"Площадь: {s}");
+            Console.WriteLine($"Объем: {v}");
+            
+
+        }
+    }
+}
---
+++ C#/lab_2/lab_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace lab_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+          
+            Console.WriteLine("Введите число k:");
+            string number = Console.ReadLine();
+            double num = Convert.ToDouble(number);
+         
+            double form_main = (Math.Pow(num, 2) - 3) / (Math.Pow(num,2)-(Math.Pow(-1, num) * num) + 3);
+            Console.WriteLine($"Результат: {form_main}");
+
+        }
+    }
+}
