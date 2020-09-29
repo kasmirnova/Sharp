4ed02f02511042c961d168811bc81cabe55785a9
Kate <58857042+kasmirnova@users.noreply.github.com>
2020-09-29 23:51:42 +0300

    Add files via upload

---
+++ C#/lab_3/lab_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+namespace lab_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введите х: ");
+            string number = Console.ReadLine();
+            int x = Convert.ToInt32(number);
+            Console.WriteLine("Введите у: ");
+            string number1 = Console.ReadLine();
+            int y = Convert.ToInt32(number1);
+            if (x < 0 && y > 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+        }
+    }
+}
