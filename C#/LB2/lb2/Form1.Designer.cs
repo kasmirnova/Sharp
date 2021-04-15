d587734489633c99e665ed58e961a05711067dd2
Kate <58857042+kasmirnova@users.noreply.github.com>
2021-04-15 22:21:16 +0300

    Add files via upload

---
+++ C#/LB2/lb2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace lb2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Deleter = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(226, 188);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 50);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(226, 259);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Deleter
+            // 
+            this.Deleter.Location = new System.Drawing.Point(392, 100);
+            this.Deleter.Name = "Deleter";
+            this.Deleter.Size = new System.Drawing.Size(134, 48);
+            this.Deleter.TabIndex = 3;
+            this.Deleter.Text = "Удалить";
+            this.Deleter.UseVisualStyleBackColor = true;
+            this.Deleter.Click += new System.EventHandler(this.Deleter_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(787, 464);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Black;
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.Deleter);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(779, 436);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(779, 436);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Deleter;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ C#/LB2/lb2/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            NumbersSet();
+
+            congrats.Text = "Ну ты терпиливый!";
+            congrats.Location = new Point(50, 250);
+            congrats.BackColor = Color.Azure;
+            congrats.Visible = false;
+            tabPage2.Controls.Add(congrats);
+            OutputButtons();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                comboBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+        }
+
+        private void Deleter_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                comboBox1.Items.Remove(textBox1.Text);
+                textBox1.Clear();
+            }
+        }
+
+
+        public static int number_button= 16;
+        public static int[] numbers = new int[number_button];
+        public static Button[] buttons = new Button[number_button];
+        static int lastB = 0;
+        Label congrats = new Label();
+
+        static void NumbersSet()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int temp;
+                do
+                {
+                    temp = (int)(new Random()).Next(1, 17);
+
+                    if (!numbers.Contains<int>(temp))
+                    {
+                        numbers[i] = temp;
+                        break;
+                    }
+                } while (numbers.Contains<int>(temp));
+            }
+        }
+        void Mix()
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Point p = buttons[i].Location;
+                    buttons[i].Location = buttons[j].Location;
+                    buttons[j].Location = p;
+                }
+            }
+        }
+
+        void Reset()
+        {
+            tabPage2.Controls.Clear();
+            lastB = 0;
+            numbers = new int[number_button];
+            NumbersSet();
+            buttons = new Button[number_button];
+            OutputButtons();
+            tabPage2.Controls.Add(congrats);
+
+        }
+        void OutputButtons()
+        {
+            int counter = 0;
+
+            for (int i = 0, j = 0; i < 16; i++, j++)
+            {
+                if (j == 4)
+                {
+                    counter++;
+                    j = 0;
+                };
+                buttons[i] = new Button();
+                buttons[i].Size = new Size(80, 80);
+                buttons[i].BackColor = Color.Black;
+                buttons[i].ForeColor = Color.White;
+                buttons[i].Location = new Point(counter * buttons[i].Width + 200, j * buttons[i].Height + 50);
+                buttons[i].Text = numbers[i].ToString();
+                buttons[i].Click += (s, e) =>
+                {
+                    congrats.Visible = false;
+                    Mix();
+
+                    Button temp = s as Button;
+                    int num = Int16.Parse(temp.Text);
+
+                    (temp).Visible = false;
+
+                    if (lastB != num - 1)
+                    {
+                        Reset();
+                    }
+                    else if (num == 16)
+                    {
+                        Reset();
+                        congrats.Visible = true;
+                    }
+                    else
+                    {
+                        lastB = num;
+                    }
+
+                };
+                tabPage2.Controls.Add(buttons[i]);
+            }
+
+        }
+    }
+}
