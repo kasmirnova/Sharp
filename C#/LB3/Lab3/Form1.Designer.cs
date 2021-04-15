1601312d3ff53b7c8ac70557afb0b4f64838943b
Kate <58857042+kasmirnova@users.noreply.github.com>
2021-04-15 21:53:42 +0300

    Add files via upload

---
+++ C#/LB3/Lab3/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(214, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.button1_PreviewKeyDown);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(757, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 325);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ХВАТИТ!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.button2_PreviewKeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(775, 343);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 106);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Еще";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.button3_PreviewKeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 47);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(233, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 45);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "НАЖМИ УЖЕ ОК!!!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ C#/LB3/Lab3/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public bool chekon = false;
+        public int eight = 1;
+        public Form1()
+        {
+            Random n = new Random();
+            int k = 300;
+            InitializeComponent();
+            button1.TabStop = false;
+            button1.Location = new Point(n.Next(550),n.Next(k));
+            button1.Width = 200;
+            button1.Height = 200;
+            label1.Text = "";
+            label1.ForeColor = Color.Transparent;
+            label2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = button1.Location.X;
+            int y = button1.Location.Y;
+            Random n = new Random();
+            int k = n.Next(5);
+            int eer = 15;
+            int gran = 400;
+            if (chekon == false)
+            {
+                if (button1.Width == 0 || button1.Height == 0)
+                {
+                    label1.ForeColor = Color.Black;
+                    label1.Text = "Кнопка “Ок” не може бути натиснута";
+                }
+                switch (k)
+                {
+
+                    case 1:
+                        if (button1.Location.X < (757 - 66))
+                        {
+                            button1.Location = new Point(x + n.Next(eer), y + n.Next(eer));
+                            button1.Width = button1.Width - n.Next(5);
+                            button1.Height = button1.Height - n.Next(5);
+                        }
+                        else
+                        {
+                            button1.Location = new Point(n.Next(gran), n.Next(gran));
+                        }
+                        break;
+                    case 2:
+                        if (button1.Location.X < (757 - 66))
+                        {
+                            button1.Location = new Point(x - n.Next(eer), y + n.Next(eer));
+                            button1.Width = button1.Width - n.Next(5);
+                            button1.Height = button1.Height - n.Next(5);
+                        }
+                        else
+                        {
+                            button1.Location = new Point(n.Next(gran), n.Next(gran));
+                        }
+                        break;
+                    case 3:
+                        if (button1.Location.X < (757 - 66))
+                        {
+                            button1.Location = new Point(x + n.Next(eer), y - n.Next(eer));
+                            button1.Width = button1.Width - n.Next(5);
+                            button1.Height = button1.Height - n.Next(5);
+                        }
+                        else
+                        {
+                            button1.Location = new Point(n.Next(gran), n.Next(gran));
+                        }
+                        break;
+                    case 4:
+                        if (button1.Location.X < (757 - 66))
+                        {
+                            button1.Location = new Point(x - n.Next(eer), y - n.Next(eer));
+                            button1.Width = button1.Width - n.Next(5);
+                            button1.Height = button1.Height - n.Next(5);
+                        }
+                        else
+                        {
+                            button1.Location = new Point(n.Next(gran), n.Next(gran));
+                        }
+                        break;
+                }
+                if (button1.Left < 50)
+                { button1.Left = 50; }
+                if ((button1.Left + button1.Width) > this.ClientSize.Width)
+                { button1.Left = this.ClientSize.Width - button1.Width; }
+                if (button1.Top < 50)
+                { button1.Top = 50; }
+                if ((button1.Top + button1.Height) > this.ClientSize.Height)
+                { button1.Top = this.ClientSize.Height - button1.Height; }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Location = new Point(100, 100);
+            button1.Width = 200;
+            button1.Height = 200;
+            label1.Text = "";
+            label1.ForeColor = Color.Transparent;
+            chekon = false;
+            eight++;
+            if(eight==8)
+            {
+                label2.Text= "НАЖМИ УЖЕ ОК!!!!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.Black;
+            label1.Text = "Кнопка ОК була натиснута!";
+            chekon = true;
+            eight = 0;
+            label2.Text = "";
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+        }
+
+        private void button1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Right || e.KeyCode == Keys.Left) //если нажали стрелку
+                e.IsInputKey = true;
+        }
+
+        private void button2_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Right || e.KeyCode == Keys.Left) //если нажали стрелку
+                e.IsInputKey = true;
+        }
+
+        private void button3_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down || e.KeyCode == Keys.Right || e.KeyCode == Keys.Left) //если нажали стрелку
+                e.IsInputKey = true;
+        }
+    }
+}
